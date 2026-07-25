949abe7bcfe35e86e4c2a825e94c2aabde1f30d1
Ramjivan <ramjivan.atr@example.com>
2026-07-25 10:58:43 +0530

    chore: remove tinacms entirely

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types.js";
-export const client = createClient({ cacheDir: 'D:/projects/actors-theater-rajasthan-jaipur/tina/__generated__/.cache/1784901886936', url: 'https://content.tinajs.io/2.4/content/dc02a629-1cb6-451d-bbb1-44a9b3279be1/github/main', token: 'd9a98dc5fb79e9517d37abe0eb62c306a3c6b672', queries,  });
-export default client;
-  
